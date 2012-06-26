fd35e3a170d5e3ae1e9b0f3284c95514caedefa1
Trey Lawrence <lawrence.trey@gmail.com>
2012-06-26 18:02:03 -0400

    Fixed safe html issue

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,9 @@
 module CommentsHelper
   def add_comment_number_links(comment)
     regex = /#[0-9]+/
-    comment.sub(regex) {|match| "<a href = #{match}>#{match}</a>".html_safe}
+    comment.sub(regex) { |match| "<a href = #{match}>#{match}</a>" }.html_safe
+  end
+  
+  def escape_chars
   end
 end
