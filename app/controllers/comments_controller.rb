c2fc6715b57d7c7814ece7fe14fc61a44bc7dd10
Trey Lawrence <lawrence.trey@gmail.com>
2012-06-29 16:14:23 -0400

    Add comment pushing and CSS styling for new pushes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,76 +1,21 @@
 class CommentsController < ApplicationController
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @comments }
-    end
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-    @comment = Comment.find(params[:id])
-  end
-
-  # GET /comments/new
-  # GET /comments/new.json
-  def new
-    @comment = Comment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @comment }
-    end
-  end
-
-  # GET /comments/1/edit
-  def edit
-    @comment = Comment.find(params[:id])
-  end
-
-  # POST /comments
-  # POST /comments.json
   def create
     @confession = Confession.find(params[:confession_id])
     @comment = @confession.comments.build(params[:comment])
+    
+    @comment_count = @confession.comments.count + 1
+    @comment_count_html = "<h3 id='reactions-heading'>Reactions (#{@comment_count})</h3>"
+    
     if @comment.save
-      flash[:success] = "Confession commiserated!"
+      Pusher['confession-channel'].trigger('new-comment-event', 
+        {:confession_id => @comment.confession_id,
+        :comment => render_to_string(@comment, locals: {count: @comment_count}),
+        :comment_count => @comment_count_html})
+        
       @confession.update_attributes(updated_at: Time.now)
       redirect_to "/confessions/#{@confession.id}/##{@confession.comments.count}"
     else
       redirect_to "/confessions/#{@confession.id}/"
-    end
-  end
-
-  # PUT /comments/1
-  # PUT /comments/1.json
-  def update
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to comments_url }
-      format.json { head :no_content }
     end
   end
 end
