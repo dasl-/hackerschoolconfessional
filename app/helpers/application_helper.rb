bade000d5612e577fb1d9e3f739d9d387930afb1
davidleibovic <david.leibovic@gmail.com>
2012-07-01 02:53:53 -0400

    a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_pusher_key
+  	if Rails.env.production?
+  	  return "'a3858e72333922ce04f6'"
+  	else
+      return "'3959abe8b63fee2973a9'"
+    end
+  end
 end
