5b3619e5c8af293f6bfc71a048acf02b49c66d9d
davidleibovic <david.leibovic@gmail.com>
2012-07-01 04:37:11 -0400

    change migration

--- db/migrate/20120626163232_change_string_to_text.rb
+++ db/migrate/20120626163232_change_string_to_text.rb
@@ -1,7 +1,7 @@
 class ChangeStringToText < ActiveRecord::Migration
   def change
-  	change_column :confessions, :content, :text, :limit => 2000
-  	change_column :comments, :content, :text, :limit => 2000
+  	change_column :confessions, :content, :text, :limit => nil
+  	change_column :comments, :content, :text, :limit => nil
   end
   def up
   end
